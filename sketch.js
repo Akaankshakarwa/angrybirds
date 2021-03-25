4da50ec8daf051acb5abf2fb5c77e5def97ed0f5
Akaankshakarwa <akaankshakarwa1998@gmail.com>
2021-03-25 12:24:15 +0530

    added

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platfrom;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,6 +18,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platfrom = new Ground(300,150,200,100);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -57,6 +59,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    platfrom.display();
     bird.display();
 }
